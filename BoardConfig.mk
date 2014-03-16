de0f71814fb6c3befad90324a05a7b87f5a2c75d
KangDroid <kanghw0720@naver.com>
2014-03-16 11:20:06 +0900

    dalilgt board configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_OTA_ASSERT_DEVICE := SHV-E120L,dalilgt
 
 # Kernel
 BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true zcache
-BOARD_KERNEL_BASE           := 0x48000000
+BOARD_KERNEL_BASE           := 0x40400000
 BOARD_KERNEL_PAGESIZE       := 2048
 BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01400000
 
