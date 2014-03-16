37c1fc01d77b522d59e70f36f93a63523f0f3cc4
Bruce <825555733@QQ.com>
2014-03-16 14:34:39 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -9,7 +9,13 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120K TARGET_DEVICE=SHV-E120K BUILD_FINGERPRINT="samsung/SHV-E120K/SHV-E120K:4.4.2/KOT49H/E120KKKJMK1:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120K-user 4.4.2 KOT49H E120KKKJMK1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120L TARGET_DEVICE=SHV-E120L BUILD_FINGERPRINT="samsung/SHV-E120L/SHV-E120L:4.4.2/KOT49H/UCLF6:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120L-user 4.4.2 KOT49H UCLF6 release-keys"
+
+
+PRODUCT_NAME := cm_e120l
+PRODUCT_DEVICE := e120l
+
+
 
 PRODUCT_NAME := cm_dalilgt
 PRODUCT_DEVICE := dalilgt
